e0f2b063cef1bfe1f9237144e721d7a622fc5b9a
softwareSouf <braha10bill@gmail.com>
2023-06-22 10:08:08 -0700

    roter

--- monty.h
+++ monty.h
@@ -70,7 +70,7 @@ void f_mod(stack_t **head, unsigned int counter);
 void f_pchar(stack_t **head, unsigned int counter);
 void f_pstr(stack_t **head, unsigned int counter);
 void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void f_rotr(stack_t **head);
 void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
